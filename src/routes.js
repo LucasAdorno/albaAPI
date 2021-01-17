b66827ed565aa3b53f048e4c9a05c45797618947
LucasAdorno <lucasadorno2702@gmail.com>
2021-01-17 18:42:52 -0300

    nova rota de query

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const ScrapeController = require("./controllers/ScrapeController.js");
 const FullDataController = require("./controllers/FullDataController.js");
+const SearchController = require("./controllers/SearchController.js");
+
 
 const routes = express.Router();
 
 routes.get("/results", ScrapeController.create);
 routes.post("/fulldata", FullDataController.create);
+routes.post("/inputsearch", SearchController.create);
+
 
 module.exports = routes;
